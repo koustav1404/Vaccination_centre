b1c0c2b72075e5cf2e6c001f2ec19a8ef3ae2716
Koustav Dhar Sarma <74303843+koustav1404@users.noreply.github.com>
2021-05-29 21:29:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,41 +25,45 @@ if (st.button('Search')==True):
 
 
 
-try :
-    
+
+try :    
     for centre in data['centers']:
-        st.write("Centre Name : ",centre['name'])
-        st.write("Address : ",centre['address']) 
-        st.write("Block Name",centre['block_name'])
-        st.write("From : ",centre['from'])
-        st.write("To : ",centre['to']) 
-        st.write("Free/Paid : ",centre['fee_type'])
         for session in centre['sessions']:
-            if session['date'] != new_date:
-                break
-            st.write("Date : ",session['date'])
-            st.write("Available Doses",session['available_capacity'])
-            st.write("Minimum Age Limit : ",session['min_age_limit'])
-            st.write("Vaccine Name : ",session['vaccine'])
-            st.write("Slots : ")
-            for slot in session['slots']:
-                st.write(slot)
-            st.write("Available capacity of First dose : ",session['available_capacity_dose1'])
-            st.write("Available capacity of Second dose : ",session['available_capacity_dose2'])
-        
-        for obj in centre['vaccine_fees']:
-            try:
+            if session['date'] == new_date:
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("")
+                st.write("Centre Name : ",centre['name'])
+                st.write("Address : ",centre['address']) 
+                st.write("Block Name : ",centre['block_name'])
+                st.write("From : ",centre['from'])
+                st.write("To : ",centre['to']) 
+                st.write("Free/Paid : ",centre['fee_type'])
+                st.write("Date : ",session['date'])
+                st.write("Available Doses",session['available_capacity'])
+                st.write("Minimum Age Limit : ",session['min_age_limit'])
+                st.write("Vaccine Name : ",session['vaccine'])
+                st.write("Slots : ")
+                for slot in session['slots']:
+                    st.write(slot)
+                st.write("Available capacity of First dose : ",session['available_capacity_dose1'])
+                st.write("Available capacity of Second dose : ",session['available_capacity_dose2'])
+                    
+
+        if 'vaccine_feees' in centre.keys():    
+            for obj in centre['vaccine_fees']:
+
                 st.write("Vaccine Name: ",obj['vaccine'])
                 st.write("Vaccine Fee : ",obj['fee'])
-            except:
-                " "
-        
+                
+                
+            
             st.write("")
             st.write("")
             st.write("")
             st.write("")
             st.write("")
-
 except:
-    " "        
-        
+    " "       
